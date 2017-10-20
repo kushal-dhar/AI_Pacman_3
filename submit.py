e60b492e86da07fa372e335924ad92fd6ed098ff
Ankur Rastogi <ankurrastogi09@gmail.com>
2017-10-20 13:21:28 -0400

    Added documentation for BackTracking

--- submit.py
+++ submit.py
@@ -3,24 +3,37 @@
 #Each of your functions should return the minimum possible L value alongside the marker positions
 #Or return -1,[] if no solution exists for the given L
 
+# implementation to check all posible orders
+# it will exclude any if that order is already present in out visited array
 def all_unique(S):
     arr = []
     length = len(S)
 
+    #loop on the full length of array
     for i in range(0, length-1):
         for j in range(i+1,length):
-            #print "visited now: ",(S.visited[j],S.visited[i])
+            #### print "visited now: ",(S[j],S[i])
+            #find the order r to check if it is unique or not
             temp = (S[j]-S[i])
-            #print "temp: arr:",(temp,arr)
+            #### print "temp: arr:",(temp,arr)
+            #### print "**********************************"
+            #if order temp is already in arr then no need to computer further
             if (temp in arr):
                 return False
+            #if temp is unique than add it in possible order array
             arr.append(temp)
     return True
 
+#implementation to find out successor functions
 def successor(visited, length, elem):
     arr = []
+    #loop for all the elements greater than elem till L
     for i in range(elem+1, length+1):
+        #Add current node to the visited array
         visited.append(i)
+        #check after adding i to visited we are getting
+        #all the unique order differences or not
+        #if yes than return all valid possible orders
         if (all_unique(visited)):
             arr.append(i)
         visited.pop()
@@ -49,15 +62,25 @@ def update_fwd_constrain(visited, check, elem):
                 check[elem+dist] = 0
     return check
 
+#bt_recursive implementation
 def bt_recursive(visited, l, m):
+    #If length of visited array is m i.e. we have found the order M
+    # elements than return visited array
     if len(visited) == m:
         return visited
-    elem = visited.pop()
-    visited.append(elem)
+
+    #Get the last element of visited array
+    elem = visited[len(visited) - 1]
+    #elem = visited.pop()
+    #visited.append(elem)
+    #Find the successor functions of visited array
     successors = successor(visited,l,elem)
     print "successors: ",successors
+    #process each child one by one
     for val in successors:
+        #append child in visited
         visited.append(val)
+        #recursively build visited array of unique orders
         if bt_recursive(visited,l,m):
             return visited
         else:
@@ -89,15 +112,21 @@ def bt_fwd_recursive(visited, fwd_check, l, m):
 
 #Your backtracking function implementation
 def BT(L, M):
-    "*** YOUR CODE HERE ***"
+    #Visited Nodes
     visited = []
+    #Successors Nodes of current Popped Item
     successors = []
 
+    #Start by appending the first node in visited array
     visited.append(0)
+    #Called bt_recursive to recursively find out all the order M combination
     ret = bt_recursive(visited, L, M)
     print "arr: ",ret
+    #check if valid orders is available or not than return
+    # M value and orders array
     if len(ret) != 0:
         return M, ret
+    #if no combination of orders present return empty array
     return -1,[]
 
 #Your backtracking+Forward checking function implementation
@@ -122,7 +151,9 @@ def CP(L, M):
 
 def main():
 
-    l,arr = FC(44,9)
+    #check for BackTracking with L = 6 and M = 4 , BT(L,M)
+    l,arr = BT(6,4)
+    # l,arr = FC(44,9)
     print "arr: ",arr
 
 if __name__ == "__main__":
